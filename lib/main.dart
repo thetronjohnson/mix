96505539618897b70cd58f8da9d94f5f181ffd50
Kiran Johns <kiranjohns369@gmail.com>
2020-04-21 19:57:55 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MixPlayer());
+
+class MixPlayer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
