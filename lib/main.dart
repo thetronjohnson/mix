49ca34fe93f8a48b4e56e69c64d7568008f5458c
Kiran Johns <kiranjohns369@gmail.com>
2020-04-21 20:17:16 +0530

    Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,34 @@ class MixPlayer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      title: 'Mix',
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon:Icon(Icons.info),
+            onPressed: (){},
+          ),
+          title: Text('Mix Player'),
+          elevation: 0.0
+          
+        ),
+        body: MixBody(),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MixBody extends StatefulWidget {
+  @override
+  _MixBodyState createState() => _MixBodyState();
+}
+
+class _MixBodyState extends State<MixBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
     );
   }
 }
